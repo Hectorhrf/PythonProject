53c67d8b9fb49af46a839bd510ae3ca20b1823f0
Hector <hrfhector@gmail.com>
2025-03-23 12:23:02 +0100

    Clase 5

---
+++ Clases/Clase5/Clase5 ejercicios.py
@@ -0,0 +1,131 @@
+#Ejercicio 1
+#Evaluación de Expresiones Booleanas
+
+expresion1 = 10>5
+expresion2 = 7+3 ==10
+expresion3 = 20<15
+expresion4 = 4*2==9
+print(expresion1, expresion2, expresion3, expresion4)
+
+#Ejercicio 2
+#Operaciones matemáticas con booleanos
+
+print(True+True)
+print(False+True)
+print(False+False)
+print(True*10)
+print("MONEY "*10)
+print(True*50)
+
+#Ejercicio 3
+#Conversión entre tipos básicos
+
+print(int(True))
+print(int(False))
+
+#Ejercicio 4
+#Propiedades de las cadenas
+
+nombre = "Hector"
+print(nombre[0])
+print(nombre[-1])
+print(nombre[4])
+print(nombre[-2])
+print(nombre[-3])
+print(nombre[-4])
+print(nombre[-5])
+
+#Ejercicio 5
+#Operaciones con cadenas y números
+
+edad = 19
+mensaje = "Tengo " + str(edad) + " años."
+print(mensaje)
+
+repetido = "Hola " * 3
+print(repetido)
+
+#Ejercicio 6
+#Operaciones con caracteres y códigos ASCII
+
+codigo = ord('A')
+print(codigo)
+
+letra = chr(66)
+print(letra)
+
+#Ejercicio 7
+#Evaluación de expresiones lógicas
+
+print(10 > 5 and 3 < 8)
+print(5 == 5 or 2 > 10)
+print(not (4 == 4 and 3 > 1))
+
+#EJERCICIOS PARA CASA
+
+#Ejercicio 1
+#Comparación de números y booleanos
+a = 14
+b = 29
+c = 47
+
+mayor = a > b
+menor = c < b
+igual = a == c
+diferente = b != c
+
+print(mayor, menor, igual, diferente)
+
+#Ejercicio 2
+#Propiedades y manipulación de cadenas
+
+frase = "Python es divertido"
+print(len(frase))
+print(frase.upper())
+print(frase.lower())
+print(frase.count("o"))
+verdadero = True
+falso = False
+
+suma = verdadero + falso
+resta = verdadero - falso
+multiplicacion = verdadero * 5
+multiplicacion_falsa = falso * 10
+print(suma, resta, multiplicacion, multiplicacion_falsa)
+
+#Ejercicio 3
+#Operaciones matemáticas con números y booleanos
+
+verdadero = True
+falso = False
+suma = verdadero + falso
+resta = verdadero - falso
+multiplicacion = verdadero * 5
+multiplicacion_falsa = falso * 10
+print(suma, resta, multiplicacion, multiplicacion_falsa)
+
+#Ejercicio 4
+#Extracción de caracteres en una cadena
+palabra = "Programación"
+print(palabra[:3])
+print(palabra[-3:])
+print(palabra[::2])
+
+#Ejercicio 5
+#Conversión de tipos y evaluación booleana
+num = 100
+num_cadena = str(num)
+print(num_cadena, type(num_cadena))
+
+cadena_num = "50"
+num_convertido = int(cadena_num)
+print(num_convertido, type(num_convertido))
+print(bool(""))
+print(bool("Texto"))
+print(bool(0))
+print(bool(1))
+print(bool(-1))
+print(bool(None))
+
+
+
---
+++ Clases/Clase5/Clase5.py
@@ -0,0 +1,68 @@
+#Ejercicio 1 Diccionario
+#Crea un diccionario que almacene información de una persona: nombre,
+#edad y si está registrado (booleano).
+
+persona = {
+    "nombre" : "Bruno",
+    "edad" : 19,
+    "registradoEnElCenso" : True
+}
+print(persona)
+
+#Ejercicio 2
+#Imprime el valor de la clave "edad" del diccionario persona.
+
+print(persona["edad"])
+
+#Ejercicio 3
+#Añade al diccionario persona la clave "ciudad" con valor "Madrid".
+
+persona["ciudad"] = "Madrid"
+print(persona)
+
+#Ejercicio 4
+#Cambia la edad de la persona a 31.
+
+persona["edad"] = 20
+print(persona)
+
+#Ejercicio 5
+#Elimina la clave "ciudad" del diccionario persona.
+
+del persona["ciudad"]
+print(persona)
+
+#Ejercicio 6
+#Crea una variable booleana que indique si existe la clave "nombre" en el diccionario persona.
+
+exsiste_nombre = "nombre" and "patata" in persona
+print(exsiste_nombre)
+
+#Ejercicio 7
+#Compara si la persona está registrada y tiene más de 18 años.
+#Guarda el resultado en una variable booleana.
+
+es_mayor_y_registrado = persona["edad"]>18 and persona["registradoEnElCenso"]
+print(es_mayor_y_registrado)
+
+#Ejercicio 8
+#Invierte el valor de "registrado" y muestra el resultado.
+
+no_registrado = not persona["registradoEnElCenso"]
+print(no_registrado)
+
+#Ejercicio 9
+#Elimina duplicados de esta lista: [1, 2, 2, 3, 3, 3] usando un conjunto.
+
+numeros = [1,2,2,3,4,5,3,4,5,2,1]
+conjunto = set(numeros)
+print(conjunto)
+
+#ejercicio 10
+#Compara si las listas [1, 2, 2, 3] y [3, 1, 2] tienen los mismos elementos únicos.
+
+lista_a=set([1,2,3,4])
+lista_b=set([4,3,2,1,5])
+
+mismos_elementos = lista_a==lista_b
+print(mismos_elementos)
